c34f4a45e2d7e49f85f1b74864e69a3965359e7c
Mihal Busho <bushomichael@gmail.com>
2021-02-26 03:13:31 -0500

    test config10

--- spec/helloWorld.spec.js
+++ spec/helloWorld.spec.js
@@ -2,6 +2,6 @@ const helloWorld = require('../src/hello');
 
 describe('helloWorld', () => {
 	it('returns hello world', () => {
-		expect(helloWorld()).toBe('hello worlds');
+		expect(helloWorld()).toBe('hello world');
 	});
 });
