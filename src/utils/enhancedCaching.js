0773114670883a95984a4b032159d87285ec9fb5
Mihal Busho <bushomichael@gmail.com>
2021-04-05 19:41:01 -0400

    set cache control

--- src/utils/enhancedCaching.js
+++ src/utils/enhancedCaching.js
@@ -3,7 +3,6 @@ import IORedis from 'ioredis';
 class EnhancedRedis extends IORedis {
 	set(key, value, options = {}) {
 		if (options.ttl) return super.set(key, value, 'ex', options.ttl);
-
 		return super.set(key, value);
 	}
 }
